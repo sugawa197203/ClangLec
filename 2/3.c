2db444be8e1d261f6cbec6fd8d402f3bdf13570c
sugawa2018616 <96975428+sugawa197203@users.noreply.github.com>
2023-12-05 15:45:14 +0900

    fix Q3

--- 2/3.c
+++ 2/3.c
@@ -3,43 +3,43 @@
 // 数字を逆順にする関数
 int Reverse(int num)
 {
-	int result = 0;
-	while (num != 0)
-	{
-		result *= 10;
-		result += num % 10;
-		num /= 10;
-	}
-	return result;
+    int a, b, c, d, e;
+    a = num % 100000 / 10000;
+    b = num % 10000 / 1000;
+    c = num % 1000 / 100;
+    d = num % 100 / 10;
+    e = num % 10;
+
+    return e * 10000 + d * 1000 + c * 100 + b * 10 + a;
 }
 
 int main()
 {
-	int m;
-	int f0 = 3, f1 = 0, f2 = 2, f3 = f1 + f0;
-	int count = 0;
+    int m;
+    int f0 = 3, f1 = 0, f2 = 2, f3 = f1 + f0;
+    int count = 0;
 
-	// 入力
-	scanf("%d", &m);
+    // 入力
+    scanf("%d", &m);
 
-	for (int i = 3; i <= m; i++)
-	{
-		f0 = f1;
-		f1 = f2;
-		f2 = f3;
-		f3 = f1 + f0;
+    for (int i = 3; i <= m; i++)
+    {
+        f0 = f1;
+        f1 = f2;
+        f2 = f3;
+        f3 = f1 + f0;
 
-		// 5 桁以上の場合は下 5 桁だけを取り出す
-		f3 %= 100000;
+        // 5 桁以上の場合は下 5 桁だけを取り出す
+        f3 %= 100000;
 
-		// 回文数かどうか判定
-		if (f3 >= 10000 && f3 == Reverse(f3))
-		{
-			count++;
-		}
-	}
+        // 回文数かどうか判定
+        if (f3 >= 10000 && f3 == Reverse(f3))
+        {
+            count++;
+        }
+    }
 
-	// 出力
-	printf("%d\n", count);
-	return 0;
+    // 出力
+    printf("%d\n", count);
+    return 0;
 }
